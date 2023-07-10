231c1cbfa8ea8ec288fb79a3722b7bb7a50d2b93
Harshitha <urshhr7760@gmail.com>
2023-07-10 22:18:14 +0530

    harshu1

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ resource "azurerm_resource_group" "rg" {
     location = "west europe"
   
 }
+resource "azurerm_virtual_network" "vnet" {
+    name = "vnet-01"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    address_space = [ "10.1.0.0/16" ]
+  
+}
