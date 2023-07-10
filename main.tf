9a89c4fe8eb0acfe5ee19ca15a4fdb5daa60cf94
Harshitha <urshhr7760@gmail.com>
2023-07-10 22:46:39 +0530

    harshu2

--- main.tf
+++ main.tf
@@ -16,3 +16,10 @@ resource "azurerm_virtual_network" "vnet" {
     address_space = [ "10.1.0.0/16" ]
   
 }
+resource "azurerm_subnet" "subnet" {
+    name ="subnet01"
+    resource_group_name = azurerm_resource_group.rg.name
+    address_prefixes = [ "10.1.1.0/24" ]
+    virtual_network_name = azurerm_virtual_network.vnet.name
+  
+}
